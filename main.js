b266c6d4e8832b99115ed65be0470aac74b88ecc
Oscar Pagani <paganio@yahoo.com>
2024-11-13 22:38:07 -0800

    added hamburger menu

---
+++ main.js
@@ -0,0 +1,13 @@
+const hamburgerButton = document.querySelector('[aria-controls="primary-nav"]');
+const nav = document.querySelector(".primary-navigation");
+
+hamburgerButton.addEventListener("click", () => {
+  // check if the navigation is opened
+  const isNavOpened = hamburgerButton.getAttribute("aria-expanded");
+
+  if (isNavOpened === "false") {
+    hamburgerButton.setAttribute("aria-expanded", "true");
+  } else {
+    hamburgerButton.setAttribute("aria-expanded", "false");
+  }
+});
